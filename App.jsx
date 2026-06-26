46854294ae6da386186f5cb5ebfc5888554edb4a
Cmoore3708-lgtm <Cmoore3708@hotmail.com>
2026-06-26 11:06:29 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+# Vecta Planner Roadmap
+
+## v1.0
+Dashboard, daily planner, 7-day planner, month view, utilisation bars, job workflow, waiting jobs, Chris tasks, search, print.
+
+## v1.1
+Website bookings into Unallocated, customer late warning, vehicle history, customer history, technician suggestions.
+
+## v2.0
+Parts waiting, turnover dashboard, labour targets, MOT reminders, customer reminders, TV display, number plate scanner.
